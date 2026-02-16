8665aaad9e0d0a22b92c6e23fe31eb13e9a8f9dd
Karim (Dell XPS 15) <karimali831@googlemail.com>
2026-02-16 18:24:11 +0000

    weird

--- src/Strategies/MicroPBV4/MicroPBV4.Risk.cs
+++ src/Strategies/MicroPBV4/MicroPBV4.Risk.cs
@@ -57,9 +57,12 @@ namespace NinjaTrader.NinjaScript.Strategies
 
             // Disable BE in strong momentum
             StrongTrend(longSide, out var trendStrengthTicks, out _, out _);
-            
+
             if (DisableBeAboveAdx > 0 && adx[0] >= DisableBeAboveAdx && trendStrengthTicks >= 200)
+            {
+                Print("[BE BYPASS] Adx: " + adx[0] + ", TrendStrengthTicks: " + trendStrengthTicks);
                 return;
+            }
 
             if (Position.MarketPosition == MarketPosition.Flat)
                 return;
