1d76d8c69829528d26e7496be1f50adfe8951eab
Karim (Dell XPS 15) <karimali831@googlemail.com>
2026-02-21 14:49:03 +0000

    Uh lala

--- src/Strategies/ApexMicroPBLiveV3/ApexMicroPBLiveV3.cs
+++ src/Strategies/ApexMicroPBLiveV3/ApexMicroPBLiveV3.cs
@@ -1215,7 +1215,7 @@ namespace NinjaTrader.NinjaScript.Strategies
         {
             // bool isRealtime = (State == State.Realtime);
             // int sig = (isRealtime && Calculate == Calculate.OnEachTick) ? 1 : 0;
-            return 1;
+            return 0;
         }
 
         private void ManageBreakEven()
